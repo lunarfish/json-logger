fbcf10207d9333e3ac3d9924e08092e7f8c2d486
Dan Jones <dan.jones@lunarfish.co.uk>
2021-03-13 12:18:20 +0000

    Correct url in setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Create a logger which outputs JSON",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/alphagov/cybersecuritytools",
+    url="https://github.com/lunarfish/json-logger",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
